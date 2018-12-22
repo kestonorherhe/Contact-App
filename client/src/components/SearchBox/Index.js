85a16bbff97af76cebf3419f63750757c766e321
keston <kestonorherhe@gmail.com>
2018-12-22 16:51:58 +0100

    All views have been modularized

--- client/src/components/SearchBox/Index.js
+++ client/src/components/SearchBox/Index.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 
 export default class SearchBox extends Component {
     render() {
-        return <div class="card-body chat-fixed-search">
-            <fieldset class="form-group position-relative has-icon-left m-0">
-                <input type="text" class="form-control" id="iconLeft" placeholder="Search user" />
-                <div class="form-control-position">
-                    <i class="ft-search"></i>
+        return <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="bug-list-search">
+                        <div class="bug-list-search-content">
+                            <form action="#">
+                                <div class="position-relative">
+                                    <input type="search" id="search-contacts" class="form-control" placeholder="Search contacts..." />
+                                    <div class="form-control-position">
+                                        <i class="la la-search text-size-base text-muted la-rotate-270"></i>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </fieldset>
+            </div>
         </div>
     }
 }
